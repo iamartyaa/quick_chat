4799a4d7e26d812f8e7e9e96990322f15cc2efc8
Amartya Yadav <iamartyayadav@gmail.com>
2022-09-03 13:01:35 +0530

    login screen body & background

--- lib/screens/login/components/body.dart
+++ lib/screens/login/components/body.dart
@@ -1,37 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quick_chat/components/rounded_button.dart';
+import 'package:quick_chat/constants.dart';
+
+import '../../../components/already_account_check.dart';
+import 'background.dart';
 
 class Body extends StatelessWidget {
   const Body({
     Key? key,
   }) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      width: double.infinity,
-      height: size.height,
-      child: Stack(
-        alignment: Alignment.center,
-        children: [
-          Positioned(
-            top: 0,
-            left: 0,
-            child: Image.asset(
-              'assets/images/main_top.png',
-              width: size.width * 0.35,
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/login.svg',
+              height: size.height * 0.3,
             ),
-          ),
-          Positioned(
-            bottom: 0,
-            right: 0,
-            child: Image.asset(
-              'assets/images/login_bottom.png',
-              width: size.width * 0.4,
+            SizedBox(height: size.height*0.03,),
+            const TextFieldContainer(
+              child: TextField(
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.person,
+                    color: kPrimaryColor,
+                  ),
+                  hintText: 'Your Email',
+                  border: InputBorder.none,
+                ),
+              ),
             ),
-          ),
-        ],
+            const TextFieldContainer(
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  icon: Icon(
+                    Icons.lock,
+                    color: kPrimaryColor,
+                  ),
+                  hintText: 'Password',
+                  border: InputBorder.none,
+                  suffixIcon: Icon(
+                    Icons.visibility,
+                    color: kPrimaryColor,
+                  ),
+                ),
+              ),
+            ),
+            RoundedButton(text: 'LOGIN', press: () {}),
+            AlreadyAccountCheck(
+              login: true,
+              press: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
