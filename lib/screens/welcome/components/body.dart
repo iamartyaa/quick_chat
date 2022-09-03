c988d0caf34fa8006189901168cafb2b53cc5e73
Amartya Yadav <iamartyayadav@gmail.com>
2022-09-03 12:25:35 +0530

    custom background

--- lib/screens/welcome/components/body.dart
+++ lib/screens/welcome/components/body.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:quick_chat/constants.dart';
+import 'package:quick_chat/screens/login/login-screen.dart';
+
+import '../../../components/rounded_button.dart';
+import 'background.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -6,21 +12,36 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: double.infinity,
-      child: Stack(
-        alignment: Alignment.center, 
-        children: [
-        Positioned(
-          top: 0,
-          left: 0,
-          child: Image.asset(
-            "assets/images/main_top.png",
-            width: size.width*0.3,
-          ),
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to QuickChat',
+              style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18),
+            ),
+            SizedBox(height: size.height*0.04,),
+            SvgPicture.asset(
+              'assets/icons/chat.svg',
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height*0.06,),
+            RoundedButton(
+              text: 'LOGIN',
+              press: (){
+                Navigator.of(context).pushNamed(LoginScreen.routeName);
+              },
+            ),
+            RoundedButton(
+              text: 'SIGN UP',
+              press: (){},
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+            ),
+          ],
         ),
-      ]),
+      ),
     );
   }
 }
